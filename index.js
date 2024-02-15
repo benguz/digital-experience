75c59f7c7082d2f9ae1aa2a656fda3a92ab54ac5
benguz <84636906+benguz@users.noreply.github.com>
2024-02-15 10:45:05 -0500

    initial

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('cursorMove', (data) => {
+        data.id = socket.id; // Assign the socket ID
+        socket.broadcast.emit('cursorMove', data);
+    });
+
+    socket.on('changeColor', (color) => {
+        io.emit('colorChanged', color);
+    });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected: ' + socket.id);
+        socket.broadcast.emit('removeCursor', { id: socket.id });
+    });
+});
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
