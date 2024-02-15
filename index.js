5dcd9ee17ab9b9ebfadb21f1f98930ff8bfb91db
benguz <84636906+benguz@users.noreply.github.com>
2024-02-15 13:54:18 -0500

    two users, obstacles, colors

--- index.js
+++ index.js
@@ -8,14 +8,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIo(server);
+let userCount = 0;
 
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    userCount++;
+    console.log(`A user connected - ${userCount} users`);
+    let userType = userCount % 2 === 0 ? 'fire' : 'water';
+    let userColor = userCount % 2 === 0 ? '#ed5555' : '#2804AA';
+    
+    socket.emit('userDetails', { userType, userColor });
 
     socket.on('cursorMove', (data) => {
         data.id = socket.id; // Assign the socket ID
+        data.type = userColor;
         socket.broadcast.emit('cursorMove', data);
     });
 
@@ -25,6 +32,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('A user disconnected: ' + socket.id);
         socket.broadcast.emit('removeCursor', { id: socket.id });
+        userCount--;
     });
 });
 
